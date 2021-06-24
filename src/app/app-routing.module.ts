9f2588898e4674e4e3def0fddf81159a9a930e3e
Thaina Biudes <thaina.biudes@gmail.com>
2021-06-24 20:10:17 -0300

    feat: configura projeto inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CadastrarComponent } from "./components/views/cadastrar/cadastrar.component";
+import { ListarComponent } from "./components/views/listar/listar.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: ListarComponent,
+    children: [{ path: "medico/listar", component: ListarComponent }],
+  },
+  {
+    path: "medico/cadastrar",
+    component: CadastrarComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
